e329c127bb270d104848a6a4b2f598369eb3be9c
jatinjangid104 <jatin.jangid.104@gmail.com>
2022-02-24 14:18:56 +0000

    Changes to syscall.c

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -8,6 +8,10 @@ static void syscall_handler(struct intr_frame*);
 
 void syscall_init(void) { intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall"); }
 
+/* static void min(int a, int b) { */
+  /* return (a < b) ? a: b; */
+/* } */
+
 static void syscall_handler(struct intr_frame* f UNUSED) {
   uint32_t* args = ((uint32_t*)f->esp);
 
@@ -18,11 +22,27 @@ static void syscall_handler(struct intr_frame* f UNUSED) {
    * include it in your final submission.
    */
 
-  /* printf("System call number: %d\n", args[0]); */
+  // printf("System call number: %d\n", args[0]);
 
   if (args[0] == SYS_EXIT) {
     f->eax = args[1];
+    printf("%s\n",thread_current()->name);
     printf("%s: exit(%d)\n", thread_current()->name, args[1]);
     thread_exit();
+  } else if(args[0] == SYS_PRACTICE ){
+
+    f->eax = (args[1] + 1);
+    /* printf("before(%d) after(%d) \n",args[1],f->eax); */
+
+  } else if (args[0] == SYS_WRITE) {
+    /* dprintf()  */
+    /* printf("args[1] = %d, args[2] = %s, args[3] = %d\n", args[1], args[2], args[3]); */
+    int fd = args[1];
+    void *buffer = args[2];
+    unsigned int size = args[3];
+    printf("%s",buffer);
+    f->eax = size; 
   }
 }
+
+
